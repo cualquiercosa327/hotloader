4d14bb1a51c3af0887cac754de00e0c5b79521ce
koekeishiya <aasvi93@hotmail.com>
2018-07-20 13:36:19 +0200

    fix sample usage

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ static HOTLOADER_CALLBACK(my_callback)
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        fprintf("Usage: %s <directory>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
